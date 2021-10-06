7cf6277b6ac18b1e94c8ea1b0c1b919828fe6bfe
PavankalyanMK <68377310+PavankalyanMK@users.noreply.github.com>
2021-10-06 17:26:53 +0530

    Add files via upload

---
+++ DBMS.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dbms;
+//import java.sql.*;
+//import com.mysql.jdbc.Connection;
+//import com.mysql.jdbc.Statement;
+import static java.awt.Event.HOME;
+//import java.util.logging.Level;
+//import java.util.logging.Logger;
+//import java.awt.Event.HOME.java;
+/**
+ *
+ * @author Pramod
+ */
+public class DBMS {
+//     private static java.sql.Connection conn;
+//    private static final String USERNAME="root";
+//    private static final String PASSWORD="JirayaTheSanin24";
+//    private static final String CONN_STRING="jdbc:mysql://localhost:3306/rtodb";
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws ClassNotFoundException {
+       
+     HOME f=new HOME();
+     f.setVisible(true);
+     
+    }
+    
+}
